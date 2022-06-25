8d66a32e795b69e7f35acdf69254c68cc25f6ccb
lyubomyr <lubomyr31@gmail.com>
2022-06-25 14:55:55 +0300

    package.json: updated dev dependencies: prettier, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: '@react-native-community',
   plugins: ['react-hooks'],
   rules: {
+    'prettier/prettier': 1,
     'react-hooks/exhaustive-deps': 0,
+    'react/no-unstable-nested-components': 0,
   },
 };
