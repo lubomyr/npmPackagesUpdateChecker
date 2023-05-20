ae66ac3dedec32d72fddcf78104a77d5b6b51374
lyubomyr <lubomyr31@gmail.com>
2023-05-20 18:19:09 +0300

    Added mac-os port

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import {name} from './app.json';
 
+const appName = Platform.OS === 'macos' ? name.toLowerCase() : name;
 AppRegistry.registerComponent(appName, () => App);
