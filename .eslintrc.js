268027d6c8db1b03561119f30b74c94b6ad61e93
lyubomyr <lubomyr31@gmail.com>
2024-08-17 17:13:23 +0300

    Fix prettier, .yarn folder added .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  plugins: ['react-hooks'],
+  plugins: ['react-hooks', 'prettier'],
   rules: {
+    'prettier/prettier': 1,
     'react-hooks/exhaustive-deps': 0,
     'react/no-unstable-nested-components': 0,
     radix: 0,
